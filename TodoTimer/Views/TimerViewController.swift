4ae99ebba42b59884a907b1eb64bab7358c6250b
H-Yuuki <yuukirinnma@gmail.com>
2022-07-03 22:21:20 +0900

    update: UIのパーツをグローバル変数として宣言

--- TodoTimer/Views/TimerViewController.swift
+++ TodoTimer/Views/TimerViewController.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 import SnapKit
+import RxCocoa
+import RxSwift
 
 class TimerViewController: UIViewController {
+    
+    var todoLabel: TodoLabel!
+    var hourLabel: TimerLabel!
+    var minuteLabel: TimerLabel!
+    var secondLabel: TimerLabel!
+    var startButton: TimerButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.init(hex: "#060523")
         setupLayout()
+        setupBinding()
     }
     
     func setupLayout() {
@@ -24,12 +33,12 @@ class TimerViewController: UIViewController {
         let textBackgroundColor = UIColor.init(hex: "#171F1A")
         //let textBackgroundColor = UIColor.init(hex: "#060523")
         
-        let todoLabel = TodoLabel(text: "TIMELAB DB設計", size: 40)
+        todoLabel = TodoLabel(text: "TIMELAB DB設計", size: 40)
         
         // 時刻表示
-        let hourLabel = TimerLabel(textColor: textColor,size: 90, frameSize: CGSize(width: screenSize.width * 0.3, height: screenSize.height * 0.15), backgroundColor: textBackgroundColor.cgColor)
-        let minuteLabel = TimerLabel(textColor: textColor,size: 90, frameSize: CGSize(width: screenSize.width * 0.3, height: screenSize.height * 0.2), backgroundColor: textBackgroundColor.cgColor)
-        let secondLabel = TimerLabel(textColor: textColor,size: 90, frameSize: CGSize(width: screenSize.width * 0.3, height: screenSize.height * 0.2), backgroundColor: textBackgroundColor.cgColor)
+        hourLabel = TimerLabel(textColor: textColor,size: 90, frameSize: CGSize(width: screenSize.width * 0.3, height: screenSize.height * 0.15), backgroundColor: textBackgroundColor.cgColor)
+        minuteLabel = TimerLabel(textColor: textColor,size: 90, frameSize: CGSize(width: screenSize.width * 0.3, height: screenSize.height * 0.2), backgroundColor: textBackgroundColor.cgColor)
+        secondLabel = TimerLabel(textColor: textColor,size: 90, frameSize: CGSize(width: screenSize.width * 0.3, height: screenSize.height * 0.2), backgroundColor: textBackgroundColor.cgColor)
         
         let timerHorizontalView = UIStackView(arrangedSubviews: [hourLabel, minuteLabel, secondLabel])
         timerHorizontalView.axis = .horizontal
@@ -40,7 +49,7 @@ class TimerViewController: UIViewController {
         secondLabel.text = "00"
         
         // スタートボタン
-        let startButton = TimerButton(textSize: 30, frameSize: CGSize(width: screenSize.width * 0.5, height: screenSize.height * 0.1), backgroundColor: textBackgroundColor)
+        startButton = TimerButton(textSize: 30, frameSize: CGSize(width: screenSize.width * 0.5, height: screenSize.height * 0.1), backgroundColor: textBackgroundColor)
         
         view.addSubview(timerHorizontalView)
         timerHorizontalView.snp.makeConstraints { make -> Void in
