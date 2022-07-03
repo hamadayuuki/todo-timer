3b4904f12cb354b9817239454c80b33e05fee3cd
H-Yuuki <yuukirinnma@gmail.com>
2022-07-03 20:41:00 +0900

    add: 初期ファイル作成

---
+++ TodoTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoTimer
+//
+//  Created by 濵田　悠樹 on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoTimerUITests/TodoTimerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TodoTimerUITestsLaunchTests.swift
+//  TodoTimerUITests
+//
+//  Created by 濵田　悠樹 on 2022/07/03.
+//
+
+import XCTest
+
+class TodoTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
